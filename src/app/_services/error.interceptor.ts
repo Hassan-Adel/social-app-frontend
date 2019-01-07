a222d004b2d0f24db696338469ac30bed7e07411
Hassan Adel <hasanadel555@hotmail.com>
2019-01-07 19:28:52 +0200

    Add error handeling 1.1 : Hadeling model state errors, server errors and unauthorized error

--- src/app/_services/error.interceptor.ts
+++ src/app/_services/error.interceptor.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpErrorResponse,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -7,29 +14,42 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ErrorInterceptor implements HttpInterceptor {
-
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(req).pipe(
-            catchError(error => {
-                if(error instanceof HttpErrorResponse) {
-                    const applicationError = error.headers.get('Application-Error');
-                    if(applicationError){
-                        console.error(applicationError);
-                        return throwError(applicationError);
-                    }
-                }
-            })
-        );
-    }
-
-
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError(error => {
+        if (error instanceof HttpErrorResponse) {
+          if (error.status === 401) {
+            return throwError(error.statusText);
+          }
+          const applicationError = error.headers.get('Application-Error');
+          if (applicationError) {
+            console.error(applicationError);
+            return throwError(applicationError);
+          }
+          const serverError = error.error;
+          let modelStateErrors = '';
+          if (serverError && typeof serverError === 'object') {
+            for (const key in serverError) {
+              if (serverError[key]) {
+                modelStateErrors += serverError[key] + '\n';
+              }
+            }
+          }
+          return throwError(modelStateErrors || serverError || 'Server Error');
+        }
+      })
+    );
+  }
 }
-// what we're effectively doing here is adding an additional HTTP interceptor to angular's array 
+// what we're effectively doing here is adding an additional HTTP interceptor to angular's array
 // of HTTP interceptors and this is our own custom one.
 // set multi to true because we don't want this to replace our existing interceptors.
 // We want this to be added to our array of already existing interceptors.
 export const ErrorInterceptorProvider = {
-    provide: HTTP_INTERCEPTORS,
-    useClass: ErrorInterceptor,
-    multi: true
+  provide: HTTP_INTERCEPTORS,
+  useClass: ErrorInterceptor,
+  multi: true
 };
