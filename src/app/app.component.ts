345dd9241ed6062ef39f9f527e09f73815e8438c
Hassan Adel <hasanadel555@hotmail.com>
2019-01-08 02:30:47 +0200

    Bug Fix 1.0 : when refresh the username disappear from the navbar because it's being set after login.. Fix : when our application first loads us we'll pluck the token out from the local storage and set the token inside our auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SocialApp-SPA';
+  jwtHelper = new JwtHelperService();
+
+  constructor(public authService: AuthService) {}
+
+  // when our application first loads us we'll pluck the token out from the local storage and set the token inside our auth service.
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
