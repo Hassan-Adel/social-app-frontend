59e971b08c25f82e79a0474947163a8bf1dce132
Hassan Adel <hasanadel555@hotmail.com>
2019-01-13 23:22:09 +0200

    Add interface User

---
+++ src/app/_models/user.ts
@@ -0,0 +1,17 @@
+// Optional properties must come After the required properties
+export interface User {
+    id: number;
+    username: string;
+    knownAs: string;
+    age: number;
+    gender: string;
+    created: Date;
+    lastActive: Date;
+    photoUrl: string;
+    city: string;
+    country: string;
+    interests?: string;
+    introduction?: string;
+    lookingFor?: string;
+    photos?: Photo[];
+}
