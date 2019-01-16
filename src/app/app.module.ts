5aa927e01920eca024199384021a45132d4852d3
Hassan Adel <hasanadel555@hotmail.com>
2019-01-17 01:57:17 +0200

    Adding Ngx-Gallery to member-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppNgxBootstrapModule } from './app-ngx-bootstrap.module';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from 'ngx-gallery';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -45,6 +46,7 @@ export function tokenGetter(){
       HttpClientModule,
       FormsModule,
       AppNgxBootstrapModule,
+      NgxGalleryModule,
       RouterModule.forRoot(appRoutes),
       JwtModule.forRoot({
          config:{
