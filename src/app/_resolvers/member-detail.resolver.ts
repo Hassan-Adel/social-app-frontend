ad4927bc1f8d0202b6c90b06147f2c16b3037d65
Hassan Adel <hasanadel555@hotmail.com>
2019-01-16 23:58:24 +0200

    Adding Route Resolver (member-detail.resolver.ts)

---
+++ src/app/_resolvers/member-detail.resolver.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { User } from '../_models/user';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { catchError } from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
+
+@Injectable({})
+export class MemberDetailResolver implements Resolve<User> {
+  constructor(
+    private userService: UserService,
+    private alertify: AlertifyService,
+    private router: Router
+  ) {}
+
+  // when we user resolve it automatically subscribes to the method so dont have to subscribe ourselves
+  resolve(route: ActivatedRouteSnapshot): Observable<User> {
+    return this.userService.getUser(route.params['id']).pipe(
+      catchError(error => {
+        this.alertify.error('Problem retrieving data');
+        this.router.navigate(['/members']);
+        return of(null); // null observable
+      })
+    );
+  }
+}
