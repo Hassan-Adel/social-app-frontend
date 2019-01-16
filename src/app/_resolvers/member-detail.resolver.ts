215ba155acf8654135d6c67aa9e55728743ce841
Hassan Adel <hasanadel555@hotmail.com>
2019-01-17 00:40:32 +0200

    Bux fix in member-detail resolver

--- src/app/_resolvers/member-detail.resolver.ts
+++ src/app/_resolvers/member-detail.resolver.ts
@@ -6,7 +6,7 @@ import { AlertifyService } from '../_services/alertify.service';
 import { catchError } from 'rxjs/operators';
 import { of, Observable } from 'rxjs';
 
-@Injectable({})
+@Injectable()
 export class MemberDetailResolver implements Resolve<User> {
   constructor(
     private userService: UserService,
