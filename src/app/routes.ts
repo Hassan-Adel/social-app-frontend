696ea5f06c39ff1772d956f8ab3597ad34f1cae3
Hassan Adel <hasanadel555@hotmail.com>
2019-01-18 04:52:49 +0200

    Update routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,6 +7,7 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 export const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -21,6 +22,7 @@ export const appRoutes: Routes = [
         component: MemberDetailComponent,
         resolve: { user: MemberDetailResolver }
       }, // user : how we're gonna access the data from our route
+      { path: 'members/edit', component: MemberEditComponent}, // we'll get the id from decoding the token 
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent }
     ]
