afcf9b00b87c7076d974b5681dfa7ff609cb3420
Hassan Adel <hasanadel555@hotmail.com>
2019-01-22 04:16:55 +0200

    Adding the main photo to the Nav bar (by getting the current user's photo url from the local storage)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './_services/auth.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -16,8 +17,12 @@ export class AppComponent implements OnInit {
   // when our application first loads us we'll pluck the token out from the local storage and set the token inside our auth service.
   ngOnInit() {
     const token = localStorage.getItem('token');
+    const user: User = JSON.parse(localStorage.getItem('user'));
     if (token) {
       this.authService.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+    if (user) {
+      this.authService.currentUser = user;
     }
   }
 }
