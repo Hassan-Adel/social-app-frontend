e89f96ae5f67ab620b0f9fec919169394b4f2547
Hassan Adel <hasanadel555@hotmail.com>
2019-01-24 02:36:47 +0200

    Fix busgs in form group

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
-import { FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { fbind } from 'q';
 
 @Component({
   selector: 'app-register',
@@ -11,18 +12,22 @@ import { FormControl, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  registerForm: FormControl;
-    constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  registerForm: FormGroup;
+    constructor(private authService: AuthService, private alertify: AlertifyService, private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.registerForm = new FormControl({
+    this.registerForm = new FormGroup({
       username: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
       confirmPassword: new FormControl('', Validators.required),
-    });
+    }, this.passwordMatchValidator);
   }
 
-  register(){
+  passwordMatchValidator(fg: FormGroup) {
+    return fg.get('password').value === fg.get('confirmPassword').value ? null : {'mismatch': true};
+  }
+
+  register() {
     // this.authService.register(this.model).subscribe(
     //   () => {
     //     this.alertify.success('regiser sucessful');
