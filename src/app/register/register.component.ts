3d25f5c76053b48a13960877c0723871a72f8751
Hassan Adel <hasanadel555@hotmail.com>
2019-01-24 02:14:46 +0200

    Adding ReactiveForms

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,19 +11,25 @@ import { AlertifyService } from '../_services/alertify.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormControl;
     constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+    this.registerForm = new FormControl({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+    });
   }
 
   register(){
-    this.authService.register(this.model).subscribe(
-      () => {
-        this.alertify.success('regiser sucessful');
-      }, error => {
-        this.alertify.error(error);
-      }
-    );
+    // this.authService.register(this.model).subscribe(
+    //   () => {
+    //     this.alertify.success('regiser sucessful');
+    //   }, error => {
+    //     this.alertify.error(error);
+    //   }
+    // );
   }
 
   cancel(){
