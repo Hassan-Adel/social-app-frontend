b1baea3835f05574618a3cf343e8392821236e9a
Hassan Adel <hasanadel555@hotmail.com>
2019-02-02 04:10:51 +0200

    A getMessages

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { PaginatedResult } from '../_models/pagination';
 import { map } from 'rxjs/operators';
+import { Message } from '../_models/message';
 
 @Injectable({
   providedIn: 'root'
@@ -77,5 +78,35 @@ export class UserService {
 
   sendLike(id: number, recipientId: number) {
     return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
+  }
+
+  getMessages(id: number, page?, itemsPerPage?, messageContainer?): Observable<PaginatedResult<Message[]>> {
+    const paginatedResult: PaginatedResult<Message[]> = new PaginatedResult<Message[]>();
+
+    let params = new HttpParams();
+
+    params = params.append('MessageContainer', messageContainer);
+
+    // page & itemsPerPage are optional becation by default the server returns he firs page with 10 items
+    if (page != null && itemsPerPage != null) {
+      params = params.append('pageNumber', page);
+      params = params.append('pageSize', itemsPerPage);
+    }
+
+    // Add observe:'response' because by default only the body is observed and we need the headers too
+    return this.http
+      .get<Message[]>(this.baseUrl + 'users/' + id + '/message', { observe: 'response', params })
+      .pipe(
+        map(response => {
+          paginatedResult.result = response.body;
+          if (response.headers.get('Pagination') != null) {
+            paginatedResult.pagination = JSON.parse(
+              response.headers.get('Pagination') // Json string to object
+            );
+          }
+          return paginatedResult;
+        })
+      );
+
   }
 }
